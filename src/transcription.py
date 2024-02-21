b1c4251232d5c21775c6bd02e5461e66104c1abe
Rodrigo Díaz Concha <rdiazconcha@hotmail.com>
2024-02-21 09:53:46 -0600

    01_04

--- src/transcription.py
+++ src/transcription.py
@@ -20,3 +20,26 @@ for file_name in ogg_files:
         temperature=0.2,
         prompt="Miss Jime, Miss Naty"
     )
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo-1106",
+        messages=[
+            { "role":"system", "content":"You are a helpful assistant."},
+            { "role":"user", "content":
+            f"""
+            Translate into English the following text that is surrounded by 3 stars (***).
+            Create a JSON document with the following elements:
+            - {file_datetime.date()}. Put the translated text here.
+            - audiofile_datetime. The value must be: {file_datetime}
+            ***
+            {transcription}
+            ***
+            """
+            }
+        ], response_format={"type":"json_object"}
+    )
+
+    with open(f"{file_datetime.date()}.json", "w") as file:
+        file.write(completion.choices[0].message.content)
+
+    print("Done!")
