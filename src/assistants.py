0781ff25f3dcc32060e0c03250dbfa0fc8bff6fa
Rodrigo Díaz Concha <rdiazconcha@hotmail.com>
2024-02-21 09:55:35 -0600

    02_02

---
+++ src/assistants.py
@@ -0,0 +1,25 @@
+import os
+from openai import OpenAI
+
+MODEL = "gpt-3.5-turbo-1106"
+
+key = os.environ.get("OPENAI_API_KEY")
+client = OpenAI(api_key=key)
+
+assistant = client.beta.assistants.create(
+    name = "KinderLogger",
+    instructions="You are an intelligent assistant.",
+    tools=[{"type":"retrieval"},{"type":"code_interpreter"}],
+    model=MODEL
+)
+
+def display_main_menu():
+    print("\n[KinderLogger Assistant]")
+    prompt=input("\nEnter your prompt: ")
+    handle_main_menu_option(prompt)
+
+def handle_main_menu_option(prompt):
+    print(prompt)
+
+while True:
+    display_main_menu()
