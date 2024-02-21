30add8302c86699632473d9c8c1c8761c76eb4f7
Rodrigo Díaz Concha <rdiazconcha@hotmail.com>
2024-02-21 09:58:15 -0600

    02_04

--- src/assistants.py
+++ src/assistants.py
@@ -1,4 +1,5 @@
 import os
+import time
 from openai import OpenAI
 
 MODEL = "gpt-3.5-turbo-1106"
@@ -26,6 +27,20 @@ def handle_main_menu_option(prompt):
         role="user",
         content=prompt
     )
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id
+    )
+    still_running = True
+    while still_running:
+        latest_run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id, run_id=run.id)
+        still_running = latest_run.status != "completed"
+        if (still_running):
+            time.sleep(2)
+
+    messages = client.beta.threads.messages.list(thread_id=thread.id)
+    print(messages.data[0].content)
 
 while True:
     display_main_menu()
